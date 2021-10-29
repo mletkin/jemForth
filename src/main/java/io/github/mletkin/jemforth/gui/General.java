f3aac22d1c2c6147ece2d00fe4139339293e9f76
mletkin <vaporized@web.de>
2021-10-29 20:33:29 +0200

    removed null check, added comments

--- src/main/java/io/github/mletkin/jemforth/gui/General.java
+++ src/main/java/io/github/mletkin/jemforth/gui/General.java
@@ -23,28 +23,66 @@ public final class General {
         // prevent instantiation
     }
 
+    /**
+     * Creates a new button.
+     * <p>
+     * All parameters are mandatory
+     *
+     * @param name
+     *                   name/text of the button
+     * @param action
+     *                   click action
+     * @param icon
+     *                   icon on the button
+     * @return a new button object
+     */
     public static JButton mkButton(String name, ActionListener action, ButtonIcon icon) {
         JButton button = new JButton(name);
         button.addActionListener(action);
         button.setIcon(icon.image());
-        button.setToolTipText(icon != null ? icon.tip() : null);
+        button.setToolTipText(icon.tip());
         button.setPreferredSize(BUTTON_DIM);
         return button;
     }
 
+    /**
+     * Creates a icon button wothout text.
+     *
+     * @param action
+     *                   click action
+     * @param icon
+     *                   icon on the button
+     * @return a new button object
+     */
     public static JButton mkButton(ActionListener action, ButtonIcon icon) {
         return mkButton("", action, icon);
     }
 
+    /**
+     * Create a verticat scroll pane for acomponent.
+     *
+     * @param component
+     *                      the component to scroll pane
+     * @return the scroll pane containing the component
+     */
     public static JScrollPane mkVerticalScrollPane(JComponent component) {
         return new JScrollPane(component, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     }
 
+    /**
+     * Create a button for a toggle action.
+     *
+     * @param action
+     *                   click action
+     * @param icon
+     *                   icon on the button
+     * @return a new button object
+     */
     public static JToggleButton mkToggleButton(ActionListener action, ButtonIcon icon) {
         JToggleButton button = new JToggleButton(icon.image());
         button.addActionListener(action);
-        button.setToolTipText(icon != null ? icon.tip() : null);
+        button.setToolTipText(icon.tip());
         button.setPreferredSize(BUTTON_DIM);
         // for future use
         // button.setBorderPainted(false);
